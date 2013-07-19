1e4ee3225648fc19469d61e8181bb2479bdd934a
Ozan Turgut <oturgut@turn.com>
2013-07-19 06:19:18 -0700

    Styling fixes
    Signed-off-by: Ozan Turgut <oturgut@turn.com>

--- dashboard/closeUpMode.js
+++ dashboard/closeUpMode.js
@@ -14,31 +14,24 @@ if (Meteor.isClient) {
 
 	Meteor.setTimeout(function() {
 		console.log("inside creative wrapper close up");
-		$('.creativesWrapper').animate({
-			width: '60%'
-		}, 2000, function() {
-			console.log(Template.closeUpMode.creatives());
-			if (Template.closeUpMode.creatives() && Template.closeUpMode.creatives().length > 0) {
-				$('.creativesWrapper .boxLabel').show();
-				var i = 0;
-				var revealCreatives = Meteor.setInterval(function() {
-					var creative = Template.closeUpMode.creatives()[i];
-					$('#' + creative.id).show();
-					i++;
-					if (i > 6) {
-						Meteor.clearInterval(revealCreatives);
-					}
-				}, 1000);
-			}
-		});
+		console.log(Template.closeUpMode.creatives());
+		/*
+		if (Template.closeUpMode.creatives() && Template.closeUpMode.creatives().length > 0) {
+			$('.creativesWrapper .boxLabel').show();
+			var i = 0;
+			var revealCreatives = Meteor.setInterval(function() {
+				var creative = Template.closeUpMode.creatives()[i];
+				$('#' + creative.id).show();
+				i++;
+				if (i > 6) {
+					Meteor.clearInterval(revealCreatives);
+				}
+			}, 1000);
+		}*/
 	}, 1000);
 
-	Meteor.setTimeout(function() {
-		$('.tags').animate({
-			width: '28%'
-		}, 2000, function() {
-			$('.tag').show();
-			$('.tags .boxLabel').show();
-		});
-	}, 1000);
+	/*Meteor.setTimeout(function() {
+		$('.tag').show();
+		$('.tags .boxLabel').show();
+	}, 1000);*/
 }
